50db2ee013ea7b65955de94b9c749eb3c6ba2489
AmanShakya <aman28515@gmail.com>
2025-07-31 06:54:49 +0545

    updated

--- azure_project/deployment.py
+++ azure_project/deployment.py
@@ -1,6 +1,7 @@
 from .settings import *
 import os 
 
+SECRET_KEY = os.environ['SECRET']
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
 CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
 DEBUG = False
